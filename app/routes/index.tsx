9ef9348bfa91e9ccd6617df33e6dcf6b8cab0d30
Ben McHone <ben@mchone.dev>
2022-03-07 22:40:09 +0000

    Cache all blog posts in memory

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,50 +1,19 @@
-import fm from 'front-matter';
-import { walk, readFile, getAuthor } from "~/utils/fs.server";
-import type { BlogPostAttributes } from "~/utils/blog-post-types";
-
 import { Link, LoaderFunction, useLoaderData } from 'remix';
+import { BlogPost, getPosts } from '~/utils/blog.server';
 
 export function meta() {
   return { title: 'Ben McHone\'s Blog' };
 }
 
 export let loader: LoaderFunction = async function() {
-  let files: BlogPost[] = [];
-
-  let walkPath = './app/routes';
-
-  const author = await getAuthor('ben-mchone');
-
-  let addFile = async (file: string) => {
-    if (file.endsWith('.md')) {
-      let frontmatter = fm<BlogPostAttributes>(await readFile(file));
-
-      files.push({
-        attributes: frontmatter.attributes,
-        body: frontmatter.attributes.excerpt.substring(0, 100) + '...',
-        url: file.substring(walkPath.length + 1, file.length - 3),
-        authorName: author.name
-      });
-    }
-  }
-
-  await walk(walkPath, addFile);
-
   return new Response(JSON.stringify({
-    blogPosts: files.sort((a, b) => b.url.localeCompare(a.url))
+    blogPosts: await getPosts()
   }), {
     headers: {
       'Content-Type': 'application/json',
       'Cache-Control': 'public, max-age=180, s-maxage=3600'
     }
   });
-}
-
-type BlogPost = {
-  attributes: BlogPostAttributes
-  body: string
-  url: string
-  authorName: string
 }
 
 type LoaderType = {
