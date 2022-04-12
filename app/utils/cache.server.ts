00fabbe872c99be2fca10a25e929e7231a20b911
Ben McHone <ben@mchone.dev>
2022-04-12 18:47:01 -0500

    Increase server cache to 10 minutes

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -4,7 +4,7 @@ declare global {
 	var __cache: TTLCache;
 }
 
-const cache = global.__cache ||= new TTLCache({ max: 10000, ttl: 1000 });
+const cache = global.__cache ||= new TTLCache({ max: 10000, ttl: 1000 * 60 * 10 });
 
 export async function get<T>(key: string, callback?: () => Promise<T> | T): Promise<T | undefined> {
 	let data = cache.get<T>(key);
