bc2463d6c6645e0d9f6a0b1e09aa6d8dd8a88d46
Ben McHone <ben@mchone.dev>
2022-04-13 21:37:08 -0500

    Remix upgrade

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -1,10 +1,10 @@
-import TTLCache from '@isaacs/ttlcache';
+import LRUCache from 'lru-cache';
 
 declare global {
-	var __cache: TTLCache;
+	var __cache: LRUCache<string, any>;
 }
 
-const cache = global.__cache ||= new TTLCache({ max: 10000, ttl: 1000 * 60 * 10 });
+const cache = global.__cache ||= new LRUCache({ max: 10000, ttl: 1000 * 60 * 10 });
 
 export async function get<T>(key: string, callback?: () => Promise<T> | T): Promise<T | undefined> {
 	let data = cache.get<T>(key);
--- remix.config.js
+++ remix.config.js
@@ -2,29 +2,10 @@
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002,
-  ignoredRouteFiles: [".*"],
-  mdx: async (filename) => {
-    const [rehypeHighlight, remarkToc, dockerfile] = await Promise.all([
-      import("rehype-highlight").then((mod) => mod.default),
-      import("remark-toc").then((mod) => mod.default),
-      import('highlight.js/lib/languages/dockerfile').then((mod) => mod.default),
-    ]);
-
-    return {
-      remarkPlugins: [remarkToc],
-      rehypePlugins: [(options = {}) => rehypeHighlight({
-        ...options,
-        languages: {
-          ...options.languages,
-          dockerfile,
-        }
-      })],
-    };
-  }
+  cacheDirectory: "./node_modules/.cache/remix",
+  ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
