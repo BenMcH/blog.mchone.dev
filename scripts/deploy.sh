30dfe19636855ae933eefe1f4f93cd5a7003dac2
Ben McHone <ben@mchone.dev>
2022-06-22 21:35:00 -0500

    Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+git pull
+docker-compose build
+docker-compose push
+kubectl apply -f k8s/0_namespace.yml
+kubectl apply -f k8s/1_deployment.yml
+kubectl apply -f k8s/2_service.yml
+kubectl apply -f k8s/3_ingress.yml
+kubectl -n rss rollout restart deployment/rss-site
