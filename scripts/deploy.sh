0468a31244910c6459332c50ad66ddc86ad8e8f7
Ben McHone <ben@mchone.dev>
2022-06-22 21:41:48 -0500

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ kubectl apply -f k8s/0_namespace.yml
 kubectl apply -f k8s/1_deployment.yml
 kubectl apply -f k8s/2_service.yml
 kubectl apply -f k8s/3_ingress.yml
-kubectl -n rss rollout restart deployment/rss-site
+kubectl -n blog rollout restart deployment/blog
